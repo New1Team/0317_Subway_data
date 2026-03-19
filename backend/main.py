2bfc6e7c502730fc63356879959e803546d07161
mpsddd-commits <mpsddd@gmail.com>
2026-03-19 10:12:46 +0900

    함수수정 및 사용법 추가

--- backend/main.py
+++ backend/main.py
@@ -4,11 +4,24 @@ from fastapi import FastAPI
 import pandas as pd
 from settings import settings
 import os
+from pydantic import BaseModel
+from typing import List, Dict
 
 app = FastAPI()
 
 spark = None
 engine_mariadb = create_engine(settings.mariadb_host)
+
+# 파일별 컬럼 매핑 모델
+class ColsMapping(BaseModel):
+  # 원본 컬럼명
+  source_col:str
+  # 변경할 컬럼명
+  target_col:str
+
+# 파일명, 매핑리스트 모델
+class FileList(BaseModel):
+  file: Dict[str, List[ColsMapping]]
 
 @app.on_event("startup")
 def startup_event():
@@ -48,42 +61,48 @@ def read_root():
   except Exception as e:
     return {"status": False, "error": str(e)}
 
-
-@app.get('/sooah')
-def read():
+# 파일마다 컬럼 정할 수 있게 만들었습니다.
+# 사용방법은 md로 넣어둘게요.
+@app.post('/coordinate')
+def read(fileCon: FileList):
   current_path = os.path.dirname(os.path.abspath(__file__))
   data_path = os.path.join(current_path, "data")
-  all_files = os.listdir(data_path)
-  for file in all_files:
-      file_path = os.path.join(data_path, file)
-      print("파일: ", file_path)
-      df = pd.read_csv(file_path, encoding="cp949", header=0, thousands=',', quotechar='"', skipinitialspace=True)
-      df.columns = df.columns.str.strip()
-      cols = ['고유역번호(외부역코드)', '위도', '경도']
-      df2 = df[cols].copy()
-      df2.columns = ['station_id', 'latitude', 'longitude']
-      print("df2:  ",df2)
-      sDf = spark.createDataFrame(df2)
-      
-      # 테이블 생성 및 적재
-      db_properties = {
-      "user": "root",
-      "password": "1234",
-      "driver": "org.mariadb.jdbc.Driver",
-      "sessionInitStatement": "SET SQL_MODE='ANSI_QUOTES'"
-      }
-      try:
-        sDf.write.jdbc(
-            url=f"{settings.db_url}?useUnicode=true&characterEncoding=UTF-8&sessionVariables=sql_mode='ANSI_QUOTES'",
-            table="coordinate",
-            mode="overwrite",
-            # 처음 테이블 생성 때는 overwrite, 추가할 땐 아래 append문
-            properties=db_properties
-        )
-        print(f"✅ {file} 적재 완료!")
-      except Exception as e:
-        print(f"😥 적재실패: {e}")
+  # all_files = os.listdir(data_path)
+  for file_name, mappings in fileCon.file.items():
+    file_path = os.path.join(data_path, file_name)
+    if not os.path.exists(file_path):
+      continue
+    print(f"✅ 처리 중인 파일: {file_path}")
 
-      check_df = spark.read.jdbc(settings.db_url, table="coordinate", properties=db_properties)
-      print("개수 ", check_df.count())
-      check_df.show()
+  df = pd.read_csv(file_path, encoding="cp949", header=0, thousands=',', quotechar='"', skipinitialspace=True)
+  df.columns = df.columns.str.strip()
+  cols = [m.source_col for m in mappings]
+  newCols = {m.source_col: m.target_col for m in mappings}
+  df2 = df[cols].copy()
+  df2 = df2.rename(columns=newCols)
+  print("df2:  ",df2)
+  sDf = spark.createDataFrame(df2)
+      
+  # 테이블 생성 및 적재
+  db_properties = {
+  "user": "root",
+  "password": "1234",
+  "driver": "org.mariadb.jdbc.Driver",
+  "sessionInitStatement": "SET SQL_MODE='ANSI_QUOTES'"
+  }
+  try:
+    sDf.write.jdbc(
+        url=f"{settings.db_url}?useUnicode=true&characterEncoding=UTF-8&sessionVariables=sql_mode='ANSI_QUOTES'",
+        table="coordinate",
+        mode="overwrite",
+        # 처음 테이블 생성 때는 overwrite, 추가할 땐 아래 append문
+        properties=db_properties
+    )
+    print(f"✅ {file_name} 적재 완료!")
+  except Exception as e:
+    print(f"😥 적재실패: {e}")
+
+  check_df = spark.read.jdbc(settings.db_url, table="coordinate", properties=db_properties)
+  print("개수 ", check_df.count())
+  check_df.show()
+
